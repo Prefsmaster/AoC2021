210b1a5b138ffd8b246922ecdf6e2e284b9105a9
Ferenc <ferencn@gmail.com>
2021-12-07 23:39:30 +0100

    day 3 part 1 done

---
+++ AoC2021/Aoc2021_03/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace AoC2021_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bitpatterns = File.ReadAllLines(@"input.txt");
+
+            Console.WriteLine(Part1(bitpatterns));
+            Console.WriteLine(Part2(bitpatterns));
+
+            Console.ReadKey();
+        }
+
+        private static int Part1(string[] patterns)
+        {
+            var gammarate = 0;
+
+            var patterncount = patterns.Length;
+            var bitsperpattern = patterns[0].Length;
+
+            var bitvalue = 1 << (bitsperpattern - 1);
+            for (var bit = 0; bit < bitsperpattern; bit++)
+            {
+                var onecount = patterns.Where((row) => row[bit] == '1').Count();
+                if (onecount > (patterncount - onecount))
+                    gammarate += bitvalue;
+                bitvalue >>= 1;
+            }
+            var epsilonrate = (1<<bitsperpattern)-1-gammarate;
+            return gammarate*epsilonrate;
+        }
+
+        private static int Part2(string[] patterns)
+        {
+            return -1;
+        }
+    }
+}
