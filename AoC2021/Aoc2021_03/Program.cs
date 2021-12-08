840ac2ea41fee91103adb0794b4310055eaf39c7
Ferenc <ferencn@gmail.com>
2021-12-08 15:01:34 +0100

    Day 3, Part 2 solved

--- AoC2021/Aoc2021_03/Program.cs
+++ AoC2021/Aoc2021_03/Program.cs
@@ -2,6 +2,7 @@
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             var bitpatterns = File.ReadAllLines(@"input.txt");
@@ -14,24 +15,37 @@
 
         private static int Part1(string[] patterns)
         {
+            var patternlist = patterns.ToList();
             var gammarate = 0;
-
-            var patterncount = patterns.Length;
             var bitsperpattern = patterns[0].Length;
 
             var bitvalue = 1 << (bitsperpattern - 1);
             for (var bit = 0; bit < bitsperpattern; bit++, bitvalue >>=1)
             {
-                if (patterns.Where((row) => row[bit] == '1').Count() * 2 > patterncount)
-                    gammarate += bitvalue;
+                if (MostOnes(patternlist,bit)) gammarate += bitvalue;
             }
             var epsilonrate = (1<<bitsperpattern)-1-gammarate;
             return gammarate*epsilonrate;
         }
-
+        private static bool MostOnes(List<string> patterns, int bit) => patterns.Where((row) => row[bit] == '1').Count() * 2 >= patterns.Count;
         private static int Part2(string[] patterns)
         {
-            return -1;
+            var oxygen_generator_rating = FilterPatterns( patterns, false);
+            var CO2_scrubber_rating = FilterPatterns(patterns, true);
+            return oxygen_generator_rating * CO2_scrubber_rating;
+        }
+
+        private static int FilterPatterns(string[] patterns, bool invertedFilter)
+        {
+            var patternlist = patterns.ToList();
+            var bitsperpattern = patterns[0].Length;
+
+            for (var bit = 0; bit < bitsperpattern && patternlist.Count > 1; bit++)
+            {
+                var tokeep = MostOnes(patternlist, bit) != invertedFilter ? '1' : '0';
+                patternlist.RemoveAll(x => x[bit] != tokeep);  // remove other patterns 
+            }
+            return Convert.ToInt32(patternlist[0], 2);
         }
     }
 }
